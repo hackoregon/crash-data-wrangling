70fa3858a802f2e4baa4baaf686e48bddf16f468
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-01-25 01:10:17 -0800

    database creation - needs to be tested on Windows

---
+++ create_database.sql
@@ -0,0 +1,12 @@
+-- Database: odot_crash_data
+
+-- DROP DATABASE odot_crash_data;
+
+CREATE DATABASE odot_crash_data
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
