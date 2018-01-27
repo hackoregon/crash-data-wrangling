30eec30e5be2900f0fb05d7f50dcd126978babb1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-01-27 02:41:51 -0800

    R script to migrate tables

---
+++ Docker/migrate.R
@@ -0,0 +1,39 @@
+get_tables <- function(mdb_file) {
+  tables <- Hmisc::mdb.get(mdb_file, tables = TRUE)
+  return(grep(pattern = "^tbl", x = tables, invert = TRUE, value = TRUE))
+}
+
+get_table <- function(mdb_file, table_name) {
+  work <- Hmisc::mdb.get(mdb_file, tables = table_name, allow = "_")
+  colnames(work) <- tolower(colnames(work))
+  return(work)
+}
+
+postgresql_connection <- function(service_name, dbname, password) {
+  return(DBI::dbConnect(
+    drv = RPostgres::Postgres(),
+    host = service_name,
+    port = 5432,
+    user = "postgres",
+    dbname = dbname,
+    password = password
+  ))
+}
+
+# connect to destination PostgreSQL server and create a database
+conn <- postgresql_connection("postgis", "postgres", postgres_password)
+DBI::dbSendStatement(conn, "CREATE DATABASE odot_crash_data;")
+DBI::dbDisconnect(conn)
+
+# reconnect to the new database
+conn <- postgresql_connection("postgis", "odot_crash_data", postgres_password)
+
+# get list of tables from MDB file
+tables <- get_tables("odot_crash_data.mdb")
+for (table_name in tables) {
+  work <- get_table("odot_crash_data.mdb", table_name)
+  DBI::dbWriteTable(conn, tolower(table_name), work)
+}
+
+# disconnect
+DBI::dbDisconnect(conn)
