a36ebf77377640a2d4e474d73285e9eaf241d948
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-03-17 02:00:24 -0700

    don't need these

--- odot_crash_data_analysis/src/data/partic_pkey.sql
+++
@@ -1,6 +0,0 @@
--- Constraint: partic_pkey
-
--- ALTER TABLE public.partic DROP CONSTRAINT partic_pkey;
-
-ALTER TABLE public.partic
-    ADD CONSTRAINT partic_pkey PRIMARY KEY (partic_id);
--- odot_crash_data_analysis/src/data/vhcl_pkey.sql
+++
@@ -1,6 +0,0 @@
--- Constraint: vhcl_pkey
-
--- ALTER TABLE public.vhcl DROP CONSTRAINT vhcl_pkey;
-
-ALTER TABLE public.vhcl
-    ADD CONSTRAINT vhcl_pkey PRIMARY KEY (vhcl_id);
